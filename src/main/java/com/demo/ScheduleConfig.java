a70baf35759d11255b51df24d44f8e1026a5b5f6
Moran Vaisberg <moran@HQ.LeadSpace.com>
2018-09-19 08:37:55 +0300

    remove AsyncJob

--- src/main/java/com/demo/ScheduleConfig.java
+++ src/main/java/com/demo/ScheduleConfig.java
@@ -5,15 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.core.LockConfiguration;
 import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.core.SimpleLock;
 import net.javacrumbs.shedlock.spring.ScheduledLockConfiguration;
 import net.javacrumbs.shedlock.spring.ScheduledLockConfigurationBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Optional;
 
 
 @Slf4j
@@ -22,7 +23,6 @@ import java.time.Instant;
 public class ScheduleConfig {
 
     private LockRepository lockRepository;
-    private AsyncJob asyncJob;
 
     private static final String LOCK_ID = "lock_01";
     private static long FIVE_MINUTES = 5;
@@ -30,8 +30,13 @@ public class ScheduleConfig {
     @Scheduled(cron = "0 */1 * * * *") // every 1 min
     public void performSomeTask () throws InterruptedException {
         log.info("task-started");
-        log.info("Lock Object: {}", lockRepository.findById(LOCK_ID));
-        asyncJob.runTask(lockProvider(),lockConfig(LOCK_ID));
+        Optional<SimpleLock> lock = lockProvider().lock(lockConfig(LOCK_ID));
+        if (lock.isPresent()) {
+            log.info("Locked, Lock Until: {}", lockRepository.findById(LOCK_ID).get().getLockedUntil());
+            Thread.sleep(5000);
+            lock.get().unlock();
+            log.info("Unlocked, Lock Until: {} ", lockRepository.findById(LOCK_ID).get().getLockedUntil());
+        }
         log.info("task-finished");
     }
 
@@ -40,7 +45,7 @@ public class ScheduleConfig {
         return ScheduledLockConfigurationBuilder
                 .withLockProvider(lockProvider)
                 .withPoolSize(10)
-                .withDefaultLockAtMostFor(Duration.ofMinutes(FIVE_MINUTES))
+                .withDefaultLockAtMostFor(Duration.ofMinutes(1))
                 .build();
     }
 
